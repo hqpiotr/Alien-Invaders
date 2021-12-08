4d4598e0317821fc5a5de992533b7cfdea6ace79
Piotr Cieslak <eu.piotr@gmail.com>
2021-12-08 16:49:02 +0100

    Initial AlienInvation class w/ default constructor window display

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,22 +2,30 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """ Main class"""
     def __init__(self):
+        """ Default constructor initializer"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((800, 600))
-        pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,
+            self.settings.screen_height))
 
-    @staticmethod
-    def run_game():
+
+    def run_game(self):
+        """ Main loop for the game """
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            """ redraw the screen """
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
 
 
---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    """ Store the settings here of the game """
+    def __init__(self):
+        # screen
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (200, 200, 200)
